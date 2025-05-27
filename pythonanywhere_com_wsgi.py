d3ea04c997e0896a857343085e9e0364323dd5d7
ItsazainBilbao <itsazainmbilbao@gmail.com>
2025-05-27 10:16:07 +0200

    segundo commit

---
+++ pythonanywhere_com_wsgi.py
@@ -0,0 +1,74 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://ItsazainBilbao2.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+
+# +++++++++++ GENERAL DEBUGGING TIPS +++++++++++
+# getting imports and sys.path right can be fiddly!
+# We've tried to collect some general tips here:
+# https://help.pythonanywhere.com/pages/DebuggingImportError
+
+
+# +++++++++++ HELLO WORLD +++++++++++
+# A little pure-wsgi hello world we've cooked up, just
+# to prove everything works.  You should delete this
+# code to get your own working.
+
+
+HELLO_WORLD = """<html>
+<head>
+    <title>PythonAnywhere hosted web application</title>
+</head>
+<body>
+<h1>Hello, World!</h1>
+<p>
+    This is the default welcome page for a
+    <a href="https://www.pythonanywhere.com/">PythonAnywhere</a>
+    hosted web application.
+</p>
+<p>
+    Find out more about how to configure your own web application
+    by visiting the <a href="https://www.pythonanywhere.com/web_app_setup/">web app setup</a> page
+</p>
+</body>
+</html>"""
+
+
+def application(environ, start_response):
+    if environ.get('PATH_INFO') == '/':
+        status = '200 OK'
+        content = HELLO_WORLD
+    else:
+        status = '404 NOT FOUND'
+        content = 'Page not found.'
+    response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(content)))]
+    start_response(status, response_headers)
+    yield content.encode('utf8')
+
+
+
+# +++++++++++ FLASK +++++++++++
+# Flask works like any other WSGI-compatible framework, we just need
+# to import the application.  Often Flask apps are called "app" so we
+# may need to rename it during the import:
+#
+#
+import sys
+#
+## The "/home/ItsazainBilbao2" below specifies your home
+## directory -- the rest should be the directory you uploaded your Flask
+## code to underneath the home directory.  So if you just ran
+## "git clone git@github.com/myusername/myproject.git"
+## ...or uploaded files to the directory "myproject", then you should
+## specify "/home/ItsazainBilbao2/myproject"
+path = "/home/ItsazainBilbao2/TallerDespliegue2503/"
+if path not in sys.path:
+    sys.path.append(path)
+#
+from api_marketing import app as application  # noqa
+#
+# NB -- many Flask guides suggest you use a file called run.py; that's
+# not necessary on PythonAnywhere.  And you should make sure your code
+# does *not* invoke the flask development server with app.run(), as it
+# will prevent your wsgi file from working.
